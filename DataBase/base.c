a208886caa9d3f2174d8f44af255bd473cbd6438
Andreas Touly <andreas.touly@gmail.com>
2015-11-01 16:52:47 +0100

    Database update

--- DataBase/base.c
+++ DataBase/base.c
@@ -12,22 +12,50 @@
 void initializeDB(EltDB *begin)
 {
     EltDB *temp = begin;
-    char *path = "../img/";
+    char *path = "img/";
     GError **error = malloc(sizeof(GError**));
-    GDir* folder = g_dir_open(path, 0, error);
-    G_CONST_RETURN gchar* file = g_dir_read_name(folder);
+    GDir* dir = g_dir_open(path, 0, error);
+    G_CONST_RETURN gchar* file = g_dir_read_name(dir);
 
     while (file != NULL)
     {
-        path = "../img/";
+        path = "img/";
         strcpy(temp->name, file);
         temp->img = load_image(strcat(path, file));
         temp->next = malloc(sizeof(EltDB*));
+        //temp->cls = recuperation StrongClassifier de l'image
         temp = temp->next;
+        file = g_dir_read_name(dir);
     }
     
-    g_dir_close(folder);
+    g_dir_close(dir);
     free(error);
+}
+
+int ajout_eltDB(EltDB *begin, gchar *name)
+{
+    EltDB *temp = begin;
+    EltDB *new = malloc(sizeof(EltDB*));
+    while(temp != NULL && *(temp->name) != *name)
+    {
+        if(*(temp->name) > *name)
+        {
+            strcpy(new->name, name);
+            new->next = temp->next;
+            temp->next = new;
+            return 1;
+        }
+        temp = temp->next;
+        
+    }
+    if (temp == NULL)
+    {
+        temp = malloc(sizeof(EltDB*));
+        strpy(temp->name, name);
+        temp->next = NULL;
+        return 1;
+    }
+    return 0;
 }
 
 int main()
--- DataBase/base.h
+++ DataBase/base.h
@@ -4,12 +4,15 @@
 #include <stdlib.h>
 #include <SDL.h>
 #include <SDL/SDL_image.h>
+#include "../Adaboost/adaboost.h"
 
 typedef struct EltDB
 {
     SDL_Surface *img;
     char *name;
+    StrongClassifier cls;
     struct EltDB *next;
+    
 } EltDB;
 
 void initializeDB(EltDB *begin);
