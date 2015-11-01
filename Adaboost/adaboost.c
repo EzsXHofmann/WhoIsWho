561e803cd33d1cd2257adcfa1c8b0016ef03ce3a
Thomas Bitzberger <thomas.bitzberger@epita.fr>
2015-11-01 05:23:07 +0100

    adaboost done pas encore debug

--- Adaboost/adaboost.c
+++ Adaboost/adaboost.c
@@ -3,66 +3,201 @@
 # include <math.h>
 # include "adaboost.h"
 
-/* Algo de la video qui correspond + ou - 
- * a celui de Wikipedia (eng) 
- */
-int adaboost(struct samples[], size_t len, size_t nbIter)
-{   
-    FILE *charFile = NULL; 
-    double sampWeights[len];
-    double weights = 1.0/len
-    size_t nbCharacs = 162336; //Nombre théorique de caracs computed
-    for(size_t i = 0; i < len; i++) //initialisation des poids
-        sampWeights[i] = weights;
+# define T 20 //nb de classifieurs faibles dans un fort
+# define NbFeatures 162336
+# define MAXFEATVAL 1000000
 
-    /* Boucle principale ; nbIter = 
-     * nombres de classificateurs faibles
-     * utilisés pour construire le classifieur
-     * fort */
-    for(size_t t; t <= nbIter; t++) 
+double tabSum(double *tab, int len)
+{
+    double acc = 0;
+    for(int i = 0; i < len; i++)
+        acc += tab[i];
+    return acc;
+        
+}
+
+int getFeature(char *featFilePath, int featNumber)
+{
+    FILE *file = fopen(featFilePath, "r");
+    if(file == NULL)
+        return -1;
+    char *s = malloc(10*sizeof(char));
+    s = fgets(s,10,file);
+    for(int i = 1; i < featNumber; i++)
     {
-       /* Normalisation des poids */
-       double  weightSum = 0.0
-       for(size_t i = 0; i < len; i++)
-            weightSum += sampWeights[i];
+        s = fgets(s,10,file);
+    }
+    int res = atoi(s);
+    free(s);
+    s = NULL;
+    return res;
 
-       weights = weights / weightSum;
-       /*Fin de la normalisation */
-       
-            /*Recuperation des features 
-             * pour le classifieur faible */
-       
-            file = fopen(samples[0].filename,"r");
-            if(file == NULL)
-            {
-                printf("Error opening %s file", samples[i].filename);
-                return -1;
-            }
-            /* Un feature par itération */
-            for(size_t j = 0; j < nbCharacs; j++)
-            {
-                char curFeat[10] = "";
-                curFeat = fgets(curFeat,5,file);
-                int feature = strtoul(curFeat,NULL,10);
+}
 
-                   
-              
-            }
-                       
+int testValue(int value, int threshold)
+{
+    if(value < threshold)
+        return 0;
+    else
+        return 1; 
+}
 
-       
+//somme de 1 à N
+double computeSum(int *values, double *weights, Sample samples[],
+                  int threshold,int N)
+{
+    double acc = 0.0;
+    for(int i = 0; i < N; i++)
+    {
+        int tested = testValue(values[i],threshold);
+        if(tested == samples[i].positive)
+            tested = 0;
+        else
+            tested = 1;
+        acc += weights[i]*(double)tested;
+    }
+    return acc;
+}
+
+// Trouve le seuil qui minimise l'erreur de classification
+int findTreshold(int *values, double *weights, Sample samples[],
+                 int N)
+{
+    
+    int min = 2000000;
+    //MAX* = valeur max possible du seuil
+    for(int i = 0; i < MAXFEATVAL; i++)
+    {
+        double res = computeSum(values,weights,samples,i,N);
+        if(res < min)
+            min = i;
     }
 
+    return min;
+
 }
 
-
-
-double epsilon(int feat, int treshold, int positive)
+void AddClassifier(StrongClassifier sc, WeakClassifier wc)
 {
-  int eps = 0;
-  if(weakClassifer(feat,threshold) != positive)
-      eps = 1;
-  return (double)epsilon; 
+    sc.wc[sc.count] = wc;
+    sc.count++;
 }
 
+int adaBoost(Sample samples[], int nbPos, int nbNeg
+            , StrongClassifier strong)
+{
+    int nbSamples = nbPos + nbNeg;
+    double *weights = malloc(nbSamples * sizeof(double));
+    int *usedFeature = malloc(nbSamples * sizeof(int));
+    
 
+    int i,j;
+
+    //initialisation des poids
+    for(i = 0; i < nbSamples; i++)
+        if(samples[i].positive == 1)
+            weights[i] = 1/nbPos;
+        else
+            weights[i] = 1/nbNeg;
+
+    //Boucle principale (boosting)
+    for(int t = 0; t < T; t++)
+    {
+        //Normalisation des poids
+        int summedWeights = tabSum(weights, nbSamples);
+        for(i = 0; i < nbSamples; i++)
+            weights[i] /= summedWeights;
+        
+        //Fin de la normalisation des poids
+
+        //Pour chaque feature on trouve train
+        //un classifieur faible hj
+        //On trouve ensuite le classifieur
+        //Avec le taux d'erreur minimal
+        WeakClassifier classifiers[NbFeatures];
+
+        for(j = 0; j < NbFeatures; j++)
+        {
+            if(usedFeature[j])
+                continue;
+            
+            int featValues[nbSamples];
+            for(i = 0; i < nbSamples; i++)
+                featValues[i] = getFeature(samples[i].filename, j);
+
+            int treshold = findTreshold(featValues, weights, samples,
+                                        nbSamples);
+            double error = computeSum(featValues, weights, samples,
+                                      treshold, nbSamples);
+
+            classifiers[j].treshold = treshold;
+            classifiers[j].error = error;
+                  
+        }
+
+        //On cherche le classifieur avec le taux
+        //d'erreur minimal
+        double minError = classifiers[0].error;
+        int minIndex = 0;
+        int treshold = 0;
+
+        for(j = 1; j < NbFeatures; j++)
+        {
+            if(classifiers[j].error < minError)
+            {
+                treshold = classifiers[j].treshold;
+                minError = classifiers[j].error;
+                minIndex = j;
+            }
+        }
+
+       
+        if(minError >= 1)
+        {
+            printf("minError >= 1 cant do log\n");
+            return -1;
+        }
+
+        //Coefficients ...
+        double beta = (1 - minError)/minError;
+        double alpha = log(beta);
+
+        WeakClassifier cls;
+        cls.treshold = treshold;
+        cls.error = minError;   
+        cls.alpha = alpha;
+        AddClassifier(strong,cls);
+        usedFeature[minIndex] = 1;
+
+        //Pondération des poids 
+        //des exemples d'apprentissage            
+        
+        for(i = 0; i < nbSamples; i++)
+        {
+            int val = getFeature(samples[i].filename, minIndex);
+            int e = testValue(val, treshold);
+
+            if(e == samples[i].positive)
+                e = 0;
+            else
+                e = 1;
+            weights[i] *= pow(beta, (double)(1-e));          
+
+        }
+
+                  
+    }
+    
+    //TODO
+    //classifieur final h(x) x = ???
+
+    free(weights);
+    free(usedFeature);
+    return 0;
+
+}
+
+int main()
+{
+    return 0;
+}
--- Adaboost/adaboost.h
+++ Adaboost/adaboost.h
@@ -1,30 +1,38 @@
 # ifndef _ADABOOST_H_
 # define _ADABOOST_H_
 
-# define T = 20 //nb de classifieurs faibles dans un fort
-# define NbFeatures = 162341
+#include <math.h>
 typedef struct
 {
     char *filename; //Haar characs computed for the sample img
-    int positive; // -1 is not a face , 1 is a face 
-} Sample
+    int positive; // 0 is not a face , 1 is a face 
+} Sample;
 typedef struct
 {
-    double treshold;
+    int treshold;
     double alpha;
     double error;
-    int positive;
-    int *features;
 
-} WeakClassifier
+} WeakClassifier;
 typedef struct 
 {
-    double error;
-    int acc;
-    WeakClassifier sc[25];
+   // double value;
+    int count;
+    WeakClassifier wc[25];
 
-} StrongClassifier
+} StrongClassifier;
     
+double tabSum(double *tab, int len);
+int getFeature(char *featFilePath, int featNumber);
+int testValue(int value, int threshold);
+double computeSum(int *values, double *weights, Sample samples[],
+                  int threshold,int N);
+int findTreshold(int *values, double *weights, Sample samples[],
+                 int N);
+void AddClassifier(StrongClassifier sc, WeakClassifier wc);
+int adaBoost(Sample samples[], int nbPos, int nbNeg
+            , StrongClassifier strong);
+
 
 
 
