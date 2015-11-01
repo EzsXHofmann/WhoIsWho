c9cdf1ab232da3e086975adb1f3b2efa3d4c6579
Andréas <andreas.touly@gmail.com>
2015-11-01 23:02:19 +0100

    Bon main etc...

--- SDLIMAGE/sat.c
+++ SDLIMAGE/sat.c
@@ -6,14 +6,17 @@
 # include "basic_fun.h"
 
 void img_integrale(SDL_Surface *img,int *sat)
-{ 
+{
+    //convert_to_grey(img);
+
     for(int i = 0; i < img->h; i++)
     {   
         int offset = i*(img->w);
         for(int j = 0; j < img->w; j++)
         {
-          Uint32  pixel = getpixel(img,i,j);
-          int pixel2 = pixel;
+          Uint8 r, g, b;
+          SDL_GetRGB(getpixel(img,i,j), img->format, &r, &g, &b);
+          int pixel2 = (int)r;
 
           if(i == 0 && j > 0)
               pixel2 += sat[j - 1];
