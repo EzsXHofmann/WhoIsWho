fd2f162af4fd0c804d571c3cd00bccc829d4a199
Andreas Touly <andreas.touly@gmail.com>
2015-12-05 23:17:22 +0100

    MàJ EIGEN

--- EIGEN/eigen.c
+++ EIGEN/eigen.c
@@ -1,68 +1,83 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 #include "../DataBase/base.h"
 #include "../SDLIMAGE/basic_fun.h"
-#include "queue.h"
+#include "eigen.h"
 
-struct queue *keepeigens(EltDB *sent, int *cp)
-{
-  struct queue *q = queue_empty();
-  EltDB *tmp = sent->next;
-
-
-void mean_images(EltDB *elt)
+int *mean_image(EltDB *elt, int *mean)
 {
   EltDB *tmp = elt->next;
-  int eigens[elt->eigen][24][24];
-  struct queue *q = queue_empty();
+  int *set = malloc(elt->eigen*576*sizeof(int));
   char *path;
   int *array;
-  int cp;
+  int cp = 0;
 
   while (tmp)
   {
-    int a = tmp->eigen;
-    cp = 0;
+    path = "../img/";
+    array = image_to_intarray(strcat(path, tmp->name));
+    
+    for (int j = 0; j < 24; j++)
+      for (int i = 0; i < 24; i++)
+        *(set + cp*576 + j*24 + i) = *(array + j*24 + i);
 
-    while (tmp->eigen == a)
-    {
-      path = "../img/";
-      array = image_to_intarray(strcat(path, tmp->name));
-      queue_push(&q, array);
-      tmp = tmp->next;
-      cp++;
-    }
-    queue_push(&q, NULL);
+    tmp = tmp->next;
+    cp++;
+  }
 
-    array = queue_pop(&q);
-
-
+  for (int j = 0; j < 24; j++)
+  {
     for (int i = 0; i < 24; i++)
     {
-      for (int j = 0; j < 24; j++)
-      {
-        while (array)
-        {
-          queue_push(&q, array);
-          eigens[a][i][j] += *(array + i + j*24);
-          array = queue_pop(&q);
-        }
-        queue_push(&q, NULL);
-        eigens[a][i][j] /= cp;
-      }
+      *(mean + j*24 + i) = 0;
+      for (int k = 0; k < cp; k++)
+        *(mean + j*24 + i) += *(set + k*576 + j*24 + i);
+
+      *(mean + j*24 + i) /= cp;
     }
   }
 
-  free(eigens);
+  return set;
 }
 
-int *difference(int *img, int *mean)
+void difference(int *set, int *mean, int nb)
 {
-  int *diff = malloc(576*sizeof(int));
   for (int j = 0; j < 24; j++)
     for (int i = 0; i < 24; i++)
-      *(diff + j*24 + i) = *(img + j*24 + i) - *(mean + j*24 + i);
-  
-  return diff;
+      for (int k = 0; k < nb; k++)
+        *(set + k*576 + j*24 + i) = *(set + k*576 + j*24 + i) - *(mean + j*24 + i);
+}
+
+/*int *transposee(int *set, int nb)
+{
+  int *trans = malloc(nb*576*sizeof(int));
+  for (int j = 0; j < 24; j++)
+    for (int i = 0; i < 24; i++)
+      for (int k = 0; k < nb; k++)
+        *(trans + k*576 + j*24 + i) = *(trans + k*56 + i*24 + j);
+
+  return trans;
+}*/
+
+int *covariance(int *set, int nb)
+{
+  int *covar = malloc(nb*576*sizeof(int));
+
+  for (int j = 0; j < 24; j++)
+    for (int i = 0; i < 24; i++)
+    {
+      for (int k = 0; k < nb; k++)
+        for (int l = 0; l < 24; l++)
+          *(covar + j*24 + i) += pow(*(set + k*576 + j*24 + i + l), 2);
+      *(covar + j*24 + i) /= nb;
+    }
+
+  return covar;
+}
+
+int main()
+{
+  return 0;
 }
